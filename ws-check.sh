5d7bc5664e1c5079ddbf3554ed7e1a48d27d03f7
Uladzislau <Uladzislau.Maltsau@itechart-group.com>
2019-02-25 12:48:55 +0300

    Typo fixed

--- ws-check.sh
+++ ws-check.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -eou pipefail
+set -eo pipefail
 
 URL=$1
 
 if [ ! -n "$URL"  ]; then
     echo -en "Specify the url to connect: "
-    while [ ! -n "$URL"  ] do
+    while [ ! -n "$URL"  ]
+    do
         read URL
     done
 fi
@@ -16,7 +17,7 @@ curl --include \
      --header "Connection: Upgrade" \
      --header "Upgrade: websocket" \
      --header "Host: example.com:80" \
-     --header "Origin: http://example.com:80" \
+     --header "Origin: https://example.com:80" \
      --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
      --header "Sec-WebSocket-Version: 13" \
      $URL
