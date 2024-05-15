1e8679a9d4aa368f8373d3597b152695e0bd7a5b
Uladzislau Maltsau <uladzislau.maltsau@digitalturbine.com>
2024-05-15 12:00:43 +0200

    git-cleanup command enhanced

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Cleans git branches that do not exist on remote anymore
 
-git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
+git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{if ($1 == "*") {print $2} else {print $1}}'`; do git branch -D $branch; done
